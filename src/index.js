93a3ffe36ee0b1d1b018f85a08c688bccec2f4ee
yahwi <yahwi32@gmail.com>
2024-09-10 10:53:53 +0700

    healthyCare-SNAPSHOT-V1.0

---
+++ src/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+
+const morgan = require("morgan");
+const route = require("./routes");
+const app = express();
+const db = require("./config/db");
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+db.connect();
+const port = 3000;
+app.use(morgan("combined"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+route(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
